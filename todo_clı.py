df9e369c9a93383c2e20cec70711cddd56490194
Caner Çakır <55649866+Canerss01@users.noreply.github.com>
2025-07-25 00:40:53 +0300

    Create todo_clı.py

---
+++ todo_clı.py
@@ -0,0 +1,34 @@
+def delcomplated(missions, compmissions):
+    while True:
+        try:
+            comp = int(input("\nPlease enter the completed mission's number:\n"))
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+            continue
+        if comp not in missions:
+            print("Please enter a valid mission number.\n")
+            return
+        compmissions[comp] = missions[comp]
+        missions.pop(comp)
+        print(f"Mission {comp} completed!\n")
+
+def main():
+    countofmis = int(input("Please enter the count of today's missions.\n"))
+    i = 1
+    missions = {}
+    compmissions = {}
+    while i <= countofmis:
+        missions[i] = input(f"Please enter the {i}. mission.\n")
+        i = i + 1
+    while missions:
+        print("\nMissions left:")
+        for key, value in missions.items():
+            print(f"{key}. {value}")
+        if compmissions:
+            print("\nComplated missions:")
+            for key, value in compmissions.items():
+                print(f"{key}. {value}")
+        delcomplated(missions,compmissions)
+    print("Congratulations you have done all missions!\n")
+
+main()
