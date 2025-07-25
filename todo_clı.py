23d6cd371e51377ff5a273cd3405509d885081cb
Caner Çakır <55649866+Canerss01@users.noreply.github.com>
2025-07-25 13:27:27 +0300

    Update todo_clı.py

--- todo_clı.py
+++ todo_clı.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 date = datetime.now()
 current_date = date.date()
-def savecomplated(task):
+def save_completed_task(task):
     try:
         file = open(f"{current_date}_dids.txt", "x")
     except FileExistsError:
         file = open(f"{current_date}_dids.txt", "a")
     file.write(str(task) + "\n")
+    print("\nTask saved to history.")
     file.close()
-    
-def delcomplated(missions, compmissions):
+
+def del_completed_task(missions, compmissions):
     while True:
         try:
             comp = int(input("\nPlease enter the completed mission's number:\n"))
@@ -21,9 +22,19 @@ def delcomplated(missions, compmissions):
             continue
         compmissions[comp] = missions[comp]
         missions.pop(comp)
-        savecomplated(compmissions[comp])
-        print(f"Mission {comp} completed!\n")
+        save_completed_task(compmissions[comp])
+        print(f"Mission {comp} completed!")
+        print(f"You have {len(missions)} mission(s) left.")
         break
+
+def show_missions(missions, compmissions):
+    print("\nMissions left:")
+    for key, value in missions.items():
+        print(f"{key}. {value}")
+    if compmissions:
+        print("\nCompleted missions:")
+        for key, value in compmissions.items():
+            print(f"{key}. {value}")
 
 def main():
     countofmis = int(input("Please enter the count of today's missions.\n"))
@@ -34,14 +45,8 @@ def main():
         missions[i] = input(f"Please enter the {i}. mission.\n")
         i = i + 1
     while missions:
-        print("\nMissions left:")
-        for key, value in missions.items():
-            print(f"{key}. {value}")
-        if compmissions:
-            print("\nComplated missions:")
-            for key, value in compmissions.items():
-                print(f"{key}. {value}")
-        delcomplated(missions,compmissions)
-    print("Congratulations you have done all missions!\n")
+        show_missions(missions, compmissions)
+        del_completed_task(missions, compmissions)
+    print(f"Congratulations! You have completed all missions!\nYou can reach your history from same file with this application. They saved to {current_date}_dids.txt")
 
 main()
